ecebfd05dd807ffdfa52252585da2d433397087e
maghrabyy <mahmoud.elmaghraby11@gmail.com>
2024-08-08 00:20:57 +0300

    Adding logo and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css';
+import logo from '../src/assets/MHLogo.png';
 
 function App() {
   return (
     <>
+      <div className="navbar py-2 px-16">
+        <img src={logo} className="w-32" alt="Market Hive logo" />
+      </div>
       <h1 className="text-4xl font-bold">MarketHive project</h1>
     </>
   );
