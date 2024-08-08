61da30eb91bcf2daf5d42fed79d572bf51428d3e
maghrabyy <mahmoud.elmaghraby11@gmail.com>
2024-08-08 23:44:52 +0300

    update homeSection comp

--- src/Components/Homepage-comp/HomeSection.jsx
+++ src/Components/Homepage-comp/HomeSection.jsx
@@ -10,10 +10,10 @@ export const HomeSection = ({
   return (
     <div className={`homepage-section pt-20 pb-4 space-y-10 ${className}`}>
       <div className="section-title flex justify-between">
-        <h2 className="text-3xl font-bold">{title}</h2>
+        <h2 className="text-lg md:text-3xl font-bold">{title}</h2>
         <Link
           to={sectionPath}
-          className="text-gray-500 hover:text-gray-400 place-self-end"
+          className="text-gray-500 hover:text-gray-400 text-sm place-self-center md:place-self-end"
         >
           View {pathTitle}
         </Link>
