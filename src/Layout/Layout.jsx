4eb7d6a736b4f6e3210fc5d91282e104d64ec8c9
maghrabyy <mahmoud.elmaghraby11@gmail.com>
2024-08-09 11:34:12 +0300

    add route guard on login & register pages

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,8 +1,20 @@
 import Navbar from '../Components/Root-Layout-Comp/NavBar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Components/Root-Layout-Comp/Footer';
+import { auth } from '../firebase';
+import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function Layout() {
+  const location = useLocation().pathname;
+  const navigate = useNavigate();
+
+  auth.onAuthStateChanged((auth) => {
+    if (auth && (location === '/login' || location === 'register')) {
+      console.log('already logged in.');
+      navigate('/');
+    }
+  });
   return (
     <>
       <Navbar />
