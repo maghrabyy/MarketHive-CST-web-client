66db10a41af5b262f0ef054d7333d36b9bc589f9
maghrabyy <mahmoud.elmaghraby11@gmail.com>
2024-08-15 15:55:09 +0300

    Add products Categegory Page & products Store Page

--- src/Pages/Products Page/ProductsCategoryPage.jsx
+++ src/Pages/Products Page/ProductsCategoryPage.jsx
@@ -1,3 +1,17 @@
+import { fetchProducts } from '../../lib/fetchProducts';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import ProductsPage from './ProductsPage';
+
 export const ProductsCategoryPage = ({ categoryId }) => {
-  return <div>Category ID: {categoryId}</div>;
+  const [categoryProducts, setCategoryProducts] = useState([]);
+  useEffect(() => {
+    const fetchCategoryProducts = async () => {
+      const fetchedProducts = await fetchProducts('categoryId', categoryId);
+      setCategoryProducts(fetchedProducts);
+    };
+    fetchCategoryProducts();
+  }, [categoryId]);
+
+  return <ProductsPage productsList={categoryProducts} />;
 };
--- src/Pages/Products Page/ProductsStorePage.jsx
+++ src/Pages/Products Page/ProductsStorePage.jsx
@@ -1,3 +1,16 @@
+import { fetchProducts } from '../../lib/fetchProducts';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import ProductsPage from './ProductsPage';
+
 export const ProductsStorePage = ({ storeId }) => {
-  return <div>store ID: {storeId}</div>;
+  const [storesProducts, setStoresProducts] = useState([]);
+  useEffect(() => {
+    const fetchStoreProducts = async () => {
+      const fetchedProducts = await fetchProducts('storeId', storeId);
+      setStoresProducts(fetchedProducts);
+    };
+    fetchStoreProducts();
+  }, [storeId]);
+  return <ProductsPage productsList={storesProducts} />;
 };
