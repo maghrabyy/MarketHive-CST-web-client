8d99ac631932f30da9c7b7a4b4faf54f45049c87
maghrabyy <mahmoud.elmaghraby11@gmail.com>
2024-08-16 17:47:09 +0300

    Finilizing productsDetails page

--- src/Pages/ProductDetail Page/ProductDetailPage.jsx
+++ src/Pages/ProductDetail Page/ProductDetailPage.jsx
@@ -1,40 +1,67 @@
-import { db } from '../../firebase';
-import { getDoc, doc } from 'firebase/firestore';
-import { useState, useEffect } from 'react';
+import { useFetchProduct } from '../../Custom Hooks/useFetchProduct';
 import { useParams } from 'react-router-dom';
-
+import { ProductDetails } from '../../Components/ProductDetails-comp/ProductDetails';
+import { ReviewForm } from '../../Components/ProductDetails-comp/ReviewForm';
+import { CustomersReviews } from '../../Components/ProductDetails-comp/CustomersReviews';
+import { Breadcrumb } from 'antd';
+import { Spin } from 'antd';
+import { FaHome, FaStore, FaShoppingBag } from 'react-icons/fa';
 function ProductDetailPage() {
   const { prodId } = useParams();
-  const [product, setProduct] = useState({});
-  const [store, setStore] = useState({});
-  const [isProductLoading, setIsProductLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchProduct = async () => {
-      try {
-        const fetchedProduct = await getDoc(doc(db, 'Products', prodId));
-        const fetchedStore = await getDoc(
-          doc(db, 'Stores', fetchedProduct.data().storeId),
-        );
-        setProduct(fetchedProduct.data());
-        setStore(fetchedStore.data());
-        setIsProductLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchProduct();
-  }, [prodId]);
-
+  const { product, store, category, productReviews, isProductLoading } =
+    useFetchProduct(prodId);
   return isProductLoading ? (
-    <p>Loading...</p>
+    <div className="h-full absolute left-1/2 flex flex-col justify-center">
+      <Spin size="large" className="self-stretch " />
+    </div>
   ) : (
-    <div className="paddingX py-5">
-      <img src={product.images[0]} alt={product.title} />
-      <h1>name: {product.title}</h1>
-      <h1>description: {product.description}</h1>
-      <h1>price: {product.price}</h1>
-      <h1>store name: {store.name}</h1>
+    <div className="product-details-page paddingX py-5 space-y-5">
+      <Breadcrumb
+        items={[
+          {
+            href: '/',
+            title: (
+              <div className="flex gap-2 items-center">
+                <FaHome />
+                <span>Home</span>
+              </div>
+            ),
+          },
+          {
+            href: `/stores/${store.id}`,
+            title: (
+              <div className="flex gap-2 items-center">
+                <FaStore />
+                <span>{store.name}</span>
+              </div>
+            ),
+          },
+          {
+            href: `/categories/${category.id}`,
+            title: (
+              <div className="flex gap-2 items-center">
+                <FaShoppingBag />
+                <span>{category.categoryName}</span>
+              </div>
+            ),
+          },
+          {
+            title: (
+              <div className="flex gap-2 items-center">
+                <FaShoppingBag />
+                <span>{product.title}</span>
+              </div>
+            ),
+          },
+        ]}
+      />
+      <ProductDetails
+        product={product}
+        store={store}
+        reviews={productReviews}
+      />
+      <ReviewForm productId={prodId} />
+      <CustomersReviews reviews={productReviews} />
     </div>
   );
 }
