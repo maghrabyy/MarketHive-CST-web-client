b3bbc1714420f11536fccb29dd5283cf70f4f4c4
maghrabyy <mahmoud.elmaghraby11@gmail.com>
2024-08-16 22:21:31 +0300

    Merged registration Data & layout scrollTo Top

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,8 +2,14 @@ import Navbar from '../Components/Root-Layout-Comp/NavBar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Components/Root-Layout-Comp/Footer';
 import { Spin } from 'antd';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export default function Layout({ isRoutesLoading }) {
+  const location = useLocation().pathname;
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, [location]);
   return (
     <>
       <Navbar />
